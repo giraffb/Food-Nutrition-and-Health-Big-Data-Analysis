7e754ccda088c13cd1ed4505fdf343b27fe9266c
giraffb <128767851+giraffb@users.noreply.github.com>
2025-08-26 17:07:02 +0800

    Add files via upload

---
+++ Food segmentation/SAM Food Segmentation.py
@@ -0,0 +1,59 @@
+import numpy as np 
+import pandas as pd 
+import os
+from matplotlib import pyplot as plt
+import torch
+import cv2
+
+pip install git+https://github.com/facebookresearch/segment-anything.git
+
+def show_anns(anns, axes=None):
+    if len(anns) == 0:
+        return
+    if axes:
+        ax = axes
+    else:
+        ax = plt.gca()
+        ax.set_autoscale_on(False)
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    polygons = []
+    color = []
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        img = np.ones((m.shape[0], m.shape[1], 3))
+        color_mask = np.random.random((1, 3)).tolist()[0]
+        for i in range(3):
+            img[:,:,i] = color_mask[i]
+            ax.imshow(np.dstack((img, m*0.5)))
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+#食物分割    
+image_path = '路径/8.png'
+image_array = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+masks = mask_generator.generate(image_array)
+
+_, axes = plt.subplots(1,3, figsize=(16,16))
+axes[0].imshow(image_array)
+show_anns(masks, axes[1])
+axes[2].imshow(image_array)
+show_anns(masks, axes[2])
